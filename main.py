bc2d612a0b15b7e28dc44594b9fa7c5ea243c71f
ahmad.faraz <ahmadfaraz196@gmail.com>
2026-01-25 13:23:53 +0530

    fix chat interface for terminal

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 from dotenv import load_dotenv
 from graph.evluator_graph import graph
-from graph.constants import HITL
+from graph.constants import HITL, EVALUATE_ANSWER
 from langchain_core.messages import HumanMessage,AIMessage
 
 logger = logging.getLogger(name=__name__)
@@ -11,6 +11,7 @@ logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler(filename="app.log",mode='w',encoding="utf-8")
 log_formatter = logging.Formatter("%(asctime)s %(name)s [%(levelname)s] %(message)s")
 file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.DEBUG)
 
 logger.addHandler(file_handler)
 
@@ -33,18 +34,18 @@ def main():
 
     # First run # The graph will stop at before HITL NODE
     logger.info("Starting graph execution")
-    for event in graph.stream(initial_state, THREAD, stream_mode="values"):
-        logger.debug(event)
-    logger.info("Reached HITL")
+    for state in graph.stream(initial_state, THREAD, stream_mode="values"):
+        logger.debug(state)
+    logger.info("Interrupted before HITL")
 
     snapshot = graph.get_state(THREAD)
     next_node = snapshot.next[0]
 
     while next_node == HITL:
-        state = graph.get_state(THREAD)
+        snapshot = graph.get_state(THREAD)
         # ---- Print AI response ----
-        if state.values["qna"]:
-            last = state.values["qna"][-1]
+        if snapshot.values["qna"]:
+            last = snapshot.values["qna"][-1]
             if isinstance(last, AIMessage):
                 print("\nAI:", last.content)
 
@@ -56,17 +57,32 @@ def main():
             as_node=HITL
         )
     
+
         # Resume after HITL # TWO PATHS: IN FIRST PATH GRAPH WILL REACH END NODE, IN SECOND PATH GRAPH WILL REACH HITL NODE
-        for _ in graph.stream(None, THREAD, stream_mode="values"):
-            pass
+        n_evaluations = len(snapshot.values["evaluations"])
+        for state in graph.stream(None, THREAD, stream_mode="values"):
+            logger.debug(state)
+            if len(state["evaluations"]) > n_evaluations:
+                last = state["evaluations"][-1]
+                n_evaluations = len(state["evaluations"])
+                print("\nAI Evaluation: ", last.content)
+
+
+        logger.info("Interrupted before HITL")
+
+        # print the evaluation
+        # snapshot = graph.get_state(THREAD)
+        # if snapshot.values["evaluations"]:
+        #     last = snapshot.values["evaluations"][-1]
+        #     print("\nAI: ", last.content)
+
 
         snapshot = graph.get_state(THREAD)
-        
         next_node = snapshot.next[0] if snapshot.next else None
 
-    state = graph.get_state(THREAD)
-    if state.values["assessment"]:
-        last = state.values["assessment"]
+    snapshot = graph.get_state(THREAD)
+    if snapshot.values["assessment"]:
+        last = snapshot.values["assessment"]
         print("\nAI:",last)
 
 if __name__ == "__main__":
